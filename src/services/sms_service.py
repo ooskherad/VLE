f6602bd4a38ceaa8561bbc7430869a128cde7165
Amir Hosein <ooskherad@gmail.com>
2022-04-30 02:39:22 +0430

    init project

---
+++ src/services/sms_service.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from kavenegar import *
+
+
+def send(receptor, message):
+    try:
+        api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
+        params = {
+            'sender': '',
+            'receptor': receptor,
+            'message': message,
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
