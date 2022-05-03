4721ae9755e4e2445f1ad8b59965615f1eac4ffd
Amir Hosein <ooskherad@gmail.com>
2022-05-03 13:18:58 +0430

    feat: models for files

--- src/home/models.py
+++ src/home/models.py
@@ -7,3 +7,22 @@ class Enumerations(models.Model):
     parent = models.ForeignKey(to="self", on_delete=models.CASCADE)
     created_by = models.ForeignKey(to=user, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class FileGroup(models.Model):
+    class Meta:
+        db_table = 'file_group'
+
+    name = models.CharField(max_length=30)
+    path = models.CharField(max_length=30)
+
+
+class Files(models.Model):
+    type = models.CharField(max_length=20)
+    format = models.CharField(max_length=20)
+    title = models.CharField(max_length=255)
+    size = models.IntegerField()
+    group_id = models.ForeignKey(to=FileGroup, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(to=user, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, default=None)
