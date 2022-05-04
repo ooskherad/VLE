c571e500773a0e0efa4a707f9f3a0adcd12520be
Amir Hosein <ooskherad@gmail.com>
2022-05-04 14:37:18 +0430

    feat: CreateCourseSubSectionView

--- src/course/api_views.py
+++ src/course/api_views.py
@@ -9,7 +9,7 @@ from permissions import IsInstructor
 from .serializers import *
 
 
-class CoursesView(LoginRequiredMixin, SerializerContext, APIView):
+class CreateCoursesView(APIView, SerializerContext):
     permission_classes = [IsInstructor, ]
     serializer_class = CourseSerializer
 
@@ -38,7 +38,7 @@ class CoursesView(LoginRequiredMixin, SerializerContext, APIView):
         return Response(data=course.data, status=status.HTTP_201_CREATED)
 
 
-class CreateCourseSection(LoginRequiredMixin, SerializerContext, APIView):
+class CreateCourseSectionView(APIView, SerializerContext):
     permission_classes = [IsInstructor, ]
     serializer_class = CourseSectionSerializer
 
@@ -56,6 +56,37 @@ class CreateCourseSection(LoginRequiredMixin, SerializerContext, APIView):
                     'course_id': self.course.id,
                     'title': data['title'],
                     'about_section': data['about_section'],
+                }
+            )
+        except Exception as error:
+            return Response(data={'error': error.args[0] + ' not found in body'}, status=status.HTTP_400_BAD_REQUEST)
+        if course_section.is_valid():
+            course_section.save()
+        else:
+            return Response(data=course_section.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data=course_section.data, status=status.HTTP_201_CREATED)
+
+
+class CreateCourseSubSectionView(APIView, SerializerContext):
+    permission_classes = [IsInstructor, ]
+    serializer_class = CourseSubSectionSerializer
+
+    def setup(self, request, *args, **kwargs):
+        self.user = request.user
+        self.course = get_object_or_404(Courses, id=request.data.get('course_id'))
+        self.course_section = get_object_or_404(CourseSections, id=request.data.get('course_section_id'))
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request):
+        data = request.data
+        if self.course.id != self.course_section.course_id:
+            return Response(data={'error': 'section does\'nt belongs to this course'}, status=status.HTTP_403_FORBIDDEN)
+        self.check_object_permissions(request, self.course)
+        try:
+            course_section = self.serializer_class(
+                data={
+                    'course_section_id': self.course.id,
+                    'title': data['title'],
                 }
             )
         except Exception as error:
--- src/course/serializers.py
+++ src/course/serializers.py
@@ -2,7 +2,7 @@ from django.utils.text import slugify
 from rest_framework import serializers
 from django.db import transaction
 
-from .models import Courses, CourseCategories, CourseOwners, CourseSections
+from .models import Courses, CourseCategories, CourseOwners, CourseSections, CourseSubSections
 from instructor.models import Instructor
 
 
@@ -34,12 +34,28 @@ class CourseSerializer(serializers.Serializer):
         slug = validated_data.get('title')
         validated_data.update(slug=slugify(slug))
         with transaction.atomic():
+            # todo: status
             course = Courses.objects.create(**validated_data)
             instructor = Instructor.objects.get(serializers.CurrentUserDefault)
             CourseOwners.objects.create(instructor=instructor)
             for category in validated_data.get('categories'):
                 CourseCategories.objects.create(course=course, category=category)
         return course
+
+    def update(self, instance, validated_data):
+        pass
+
+
+class CourseSubSectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField()
+    course_section_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CourseSubSections.objects.create(
+            course_section_id=validated_data['course_section_id'],
+            title=validated_data['title'],
+        )
 
     def update(self, instance, validated_data):
         pass
