3bcb97e2dfedbc0454a8dfa60cf66faa209e8336
Amir Hosein <ooskherad@gmail.com>
2022-05-23 14:49:49 +0430

    feat course count for category

--- src/home/api_views.py
+++ src/home/api_views.py
@@ -1,11 +1,13 @@
 from helpers import CreateAbstract as Create
 from home.serializers import CategorySerializer, EnumerationSerializer
 from permissions import IsInstructor
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Category
+from course.models import CourseCategories
+from django.db.models import Count
 
 
 class CreateAbstract(Create):
@@ -34,6 +36,7 @@ class CreateEnumerations(CreateAbstract):
 
 
 class GetCategories(APIView):
+
     def get(self, request):
         """
 
@@ -41,10 +44,17 @@ class GetCategories(APIView):
         :return: categories with details
         """
         parents = Category.objects.filter(parent__isnull=True)
-        serializer = CategorySerializer(instance=parents, many=True)
-        data = serializer.data
-        for parent in data:
-            category_serializer = CategorySerializer(instance=Category.objects.filter(parent_id=parent.get('id')),
-                                                     many=True)
-            parent.update(categoies=category_serializer.data)
+        data = {}
+        for parent in parents:
+            parent_categories = Category.objects.filter(parent_id=parent.id)
+            parent_data = CategorySerializer(instance=parent).data
+            parent_course_count = 0
+            for category in parent_categories:
+                categories_count = CourseCategories.objects.filter(category_id=category.id).aggregate(Count('course'))
+                parent_course_count += categories_count['course__count']
+                serializer = CategorySerializer(instance=category).data
+                serializer.update(categories_count)
+                parent_data.update(category=serializer)
+            parent_data.update({'parent_course_count': 0})
+            data.update(parent_data)
         return Response(data, status=status.HTTP_200_OK)
